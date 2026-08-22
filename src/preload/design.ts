2b567575372175603495b29b2c3e46fc2d174690
zila-kh <silasmartboy2015@gmail.com>
2026-08-22 18:33:36 +0700

    feat(design): bridge managed dev preview

--- src/preload/design.ts
+++ src/preload/design.ts
@@ -5,6 +5,7 @@ const api: DesignDesktopApi = {
   state: () => ipcRenderer.invoke(DESIGN_IPC.state),
   refresh: () => ipcRenderer.invoke(DESIGN_IPC.refresh),
   previewHtml: (path) => ipcRenderer.invoke(DESIGN_IPC.previewHtml, path),
+  startDevPreview: () => ipcRenderer.invoke(DESIGN_IPC.startDevPreview),
   stopPreview: () => ipcRenderer.invoke(DESIGN_IPC.stopPreview),
 }
 
