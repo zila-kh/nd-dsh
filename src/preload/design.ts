484adabf6edbb64b45ea79775f1a7df1d423f6e9
zila-kh <silasmartboy2015@gmail.com>
2026-08-22 18:29:28 +0700

    feat(design): bridge design source API

---
+++ src/preload/design.ts
@@ -0,0 +1,11 @@
+import { contextBridge, ipcRenderer } from 'electron'
+import { DESIGN_IPC, type DesignDesktopApi } from '../shared/design.js'
+
+const api: DesignDesktopApi = {
+  state: () => ipcRenderer.invoke(DESIGN_IPC.state),
+  refresh: () => ipcRenderer.invoke(DESIGN_IPC.refresh),
+  previewHtml: (path) => ipcRenderer.invoke(DESIGN_IPC.previewHtml, path),
+  stopPreview: () => ipcRenderer.invoke(DESIGN_IPC.stopPreview),
+}
+
+contextBridge.exposeInMainWorld('ndDshDesign', api)
